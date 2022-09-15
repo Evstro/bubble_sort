147948a1c079b44f63464ad871c537b722917f68
Evstro <Evstro.code@gmail.com>
2022-09-15 17:03:56 -0500

    Complete
    Added a variable which will sort the array based on the length of the
    array - 1 times.
    Can still reduce the amount of sorts but is working properly.
    
    [Ticket: X]

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,23 +1,22 @@
 # methods
 def bubble_sort(number_array)
   number_array_length = number_array.length
-  times_to
-  for number in number_array
-    current_number_index = number_array.find_index(number)
-    next_number_index = current_number_index + 1
-    current_number = number_array[current_number_index]
-    next_number = number_array[next_number_index]
-    puts "current: #{current_number}"
-    puts "next: #{next_number}"
-      
-    if next_number_index < number_array_length
-      if current_number > next_number
-        number_array[current_number_index] = next_number
-        number_array[next_number_index] = current_number
+  times_to_sort = number_array_length - 1
+  times_to_sort.times do
+    for number in number_array
+      current_number_index = number_array.find_index(number)
+      next_number_index = current_number_index + 1
+      current_number = number_array[current_number_index]
+      next_number = number_array[next_number_index]
+        
+      if next_number_index < number_array_length
+        if current_number > next_number
+          number_array[current_number_index] = next_number
+          number_array[next_number_index] = current_number
+        end
       end
-    end
 
-    p number_array
+    end
   end
   
   p number_array
