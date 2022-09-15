cd92215048d8f4b8d5ce48b7b388ae7f8b0c6979
Evstro <Evstro.code@gmail.com>
2022-09-15 16:48:08 -0500

    Single Pass Through
    Created a for loop that compares each number with the next number and
    swaps places in the array.
    Returns an error when doing the last number as there is not an item with
    at that index
    Will need to implement something to stop at the next to last number
    
    [Ticket: X]

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,21 @@
 # methods
 def bubble_sort(number_array)
-
+  
+  for number in number_array
+    current_number_index = number_array.find_index(number)
+    next_number_index = current_number_index + 1
+    current_number = number_array[current_number_index]
+    next_number = number_array[next_number_index]
+    puts "current: #{current_number}"
+    puts "next: #{next_number}"
+    if current_number > next_number
+      number_array[current_number_index] = next_number
+      number_array[next_number_index] = current_number
+    end
+    p number_array
+  end
+  
+  p number_array
 end
 
 # variables
