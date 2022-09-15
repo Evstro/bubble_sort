414acc0ed775c50600c95e08f5fb2e9c573f2fca
Evstro <Evstro.code@gmail.com>
2022-09-15 16:13:35 -0500

    Initial Commit
    Added to README and added ruby file and outline
    
    [Ticket: X]

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bubble_sort
+
+The goal of this program is to take an array of numbers and sort the numbers from lowest to highest
---
+++ bubble_sort.rb
@@ -0,0 +1,14 @@
+# methods
+def bubble_sort(number_array)
+
+end
+
+# variables
+
+
+# program
+
+bubble_sort([4,3,78,2,0,2])
+
+# expected return
+# [0,2,2,3,4,78]
